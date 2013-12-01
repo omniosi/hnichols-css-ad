1f6f0638d8c59193184c35be73941fa92f8e48d4
ornette coleman <omniosi@gmail.com>
2013-12-01 17:56:42 -0500

    added mobile specific styles

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-console.log("script attached");
+// console.log("script attached");
 
-document.getElementById("reveal").style.opacity = 0;
+var width = document.body.clientWidth;
+// console.log(width);
+
+if(width > 500){
+	document.getElementById("reveal").style.opacity = 0;
+}
 
 function hideshow(which){
 	if (!document.getElementById)
 		return
+
 	if (which.style.opacity == 100) 
 		which.style.opacity=0
 	else
-		which.style.opacity=100
-	
+		which.style.opacity=100	
 }
